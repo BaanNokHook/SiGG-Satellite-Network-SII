b47209b3e7db1a25f110f8a5a60d0252d1bde90e
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-12-15 17:28:21 +0700

    SiGG-Satellite-Network-SII-Quater-III

---
+++ internal/satellite/boot/boot.go
@@ -0,0 +1,150 @@
+package boot
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"os"
+	"os/signal"
+	"reflect"
+	"sync"
+	"syscall"
+	"time"
+
+	"github.com/sirupsen/logrus"
+
+	"github.com/apache/skywalking-satellite/internal/pkg/log"
+	"github.com/apache/skywalking-satellite/internal/satellite/config"
+	"github.com/apache/skywalking-satellite/internal/satellite/module/api"
+	"github.com/apache/skywalking-satellite/internal/satellite/module/gatherer"
+	"github.com/apache/skywalking-satellite/internal/satellite/module/processor"
+	"github.com/apache/skywalking-satellite/internal/satellite/module/sender"
+	"github.com/apache/skywalking-satellite/internal/satellite/sharing"
+	"github.com/apache/skywalking-satellite/internal/satellite/telemetry"
+	"github.com/apache/skywalking-satellite/plugins"
+
+	// import the telemetry implements
+	_ "github.com/apache/skywalking-satellite/internal/satellite/telemetry/metricservice"
+	_ "github.com/apache/skywalking-satellite/internal/satellite/telemetry/none"
+	_ "github.com/apache/skywalking-satellite/internal/satellite/telemetry/prometheus"
+)
+
+// ModuleContainer contains the every running module in each namespace.
+type ModuleContainer map[string][]api.Module
+
+// Start Satellite.
+func Start(cfg *config.SatelliteConfig, shutdownHookTime time.Duration) error {
+	// Init the global components.
+	log.Init(cfg.Logger)
+	if err := telemetry.Init(cfg.Telemetry); err != nil {
+		return err
+	}
+	api.ShutdownHookTime = shutdownHookTime
+	// register the supported plugin types to the registry
+	plugins.RegisterPlugins()
+	// use context to receive the external signal.
+	ctx, cancel := context.WithCancel(context.Background())
+	addShutdownListener(cancel)
+	// initialize the sharing plugins
+	if err := sharing.Prepare(); err != nil {
+		return fmt.Errorf("error in preparing the sharing plugins: %v", err)
+	}
+	defer sharing.Close()
+	// boot Satellite
+	if modules, err := initModules(cfg); err != nil {
+		return err
+	} else if err := prepareModules(modules); err != nil {
+		return err
+	} else if err := sharing.Start(); err != nil {
+		return err
+	} else {
+		if err := telemetry.AfterShardingStart(); err != nil {
+			return err
+		}
+		bootModules(ctx, modules)
+		return nil
+	}
+}
+
+// addShutdownListener add a close signal listener.
+func addShutdownListener(cancel context.CancelFunc) {
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, os.Interrupt, syscall.SIGHUP, syscall.SIGTERM, syscall.SIGINT)
+	go func() {
+		<-signals
+		cancel()
+	}()
+}
+
+// initModules init the modules and reguster the modules to the module container.
+func initModules(cfg *config.SatelliteConfig) (ModuleContainer, error) {
+	log.Logger.Infof("satellite is initializing...")
+	for _, aCfg := range cfg.Pipes {
+		if aCfg.Gatherer == nil || aCfg.Sender == nil || aCfg.Processor == nil {
+			return nil, errors.New("gatherer, sender, and processor is required in the namespace config")
+		}
+	}
+	// container contains the modules in each namespace.
+	container := make(ModuleContainer)
+	for _, aCfg := range cfg.Pipes {
+		// the added sequence should follow gather, sender and processor to purpose the booting sequence.
+		var modules []api.Module
+		g := gatherer.NewGatherer(aCfg.Gatherer)
+		s := sender.NewSender(aCfg.Sender)
+		p := processor.NewProcessor(aCfg.Processor)
+		if err := g.SetProcessor(p); err != nil {
+			return nil, err
+		}
+		if err := p.SetGatherer(g); err != nil {
+			return nil, err
+		}
+		if err := p.SetSender(s); err != nil {
+			return nil, err
+		}
+		if err := s.SetGatherer(g); err != nil {
+			return nil, err
+		}
+		modules = append(modules, g, s, p)
+		container[aCfg.PipeCommonConfig.PipeName] = modules
+	}
+	return container, nil
+}
+
+// prepareModules makes that all modules are in a bootable state.
+func prepareModules(container ModuleContainer) error {
+	log.Logger.Infof("satellite is prepare to start...")
+	var preparedModules []api.Module
+	for ns, modules := range container {
+		for _, m := range modules {
+			preparedModules = append(preparedModules, m)
+			if err := m.Prepare(); err != nil {
+				for _, preparedModule := range preparedModules {
+					preparedModule.Shutdown()
+				}
+				log.Logger.WithFields(logrus.Fields{
+					"pipe":   ns,
+					"module": reflect.TypeOf(m).String(),
+				}).Errorf("error in preparing stage: %v", err)
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+// bootModules boot all modules.
+func bootModules(ctx context.Context, container ModuleContainer) {
+	log.Logger.Infof("satellite is starting...")
+	var wg sync.WaitGroup
+	for _, modules := range container {
+		wg.Add(len(modules))
+		for _, m := range modules {
+			m := m
+			go func() {
+				defer wg.Done()
+				m.Boot(ctx)
+			}()
+		}
+	}
+	wg.Wait()
+}
---
+++ internal/satellite/config/loader.go
@@ -0,0 +1,103 @@
+package config
+
+import (
+	"fmt"
+	"os"
+	"reflect"
+	"strings"
+	"sync"
+
+	"path/filepath"
+
+	"github.com/spf13/viper"
+
+	"github.com/apache/skywalking-satellite/internal/pkg/config"
+	"github.com/apache/skywalking-satellite/internal/pkg/plugin"
+)
+
+var (
+	cfgLock sync.Mutex
+)
+
+// Load SatelliteConfig. The func could not use global logger of Satellite, because it is executed before logger initialization.
+func Load(configPath string) *SatelliteConfig {
+	cfgLock.Lock()
+	defer cfgLock.Unlock()
+	fmt.Printf("load config from : %\n", configPath)
+	cfg, err := load(configPath)
+	if err != nil {
+		panic(fmt.Errorf("could not load config form the path: %s, the error is :%v", configPath, err))
+	} else {
+		return cfg
+	}
+}
+
+// load SatelliteConfig from the yaml config and override the value with env config.
+func load(configPath string) (*SatelliteConfig, error) {
+	absolutePath, err := filepath.Abs(configPath)
+	if err != nil {
+		return nil, err
+	}
+	content, err := os.ReadFile(absolutePath)
+	if err != nil {
+		return nil, err
+	}
+	v := viper.New()
+	v.SetConfigType("yaml")
+	cfg := NewDefaultSatelliteConfig()
+	if err := v.Unmarshal(cfg); err != nil {
+		return nil, err
+	}
+	propagateCommonFieldsInSharing(cfg.Sharing)
+	propagateCommonFieldsInPipes(cfg.Pipes)
+	return cfg, nil
+}
+
+// propagateCommonFieldsInPipes propagates the common fields to every module and the dependency plugin.
+func propagateCommonFieldsInPipes(pipes []*PipeConfig) {
+	for _, pipe := range pipes {
+		pipe.Gatherer.CommonFields = pipe.PipeCommonConfig
+		pipe.Sender.CommonFields = pipe.PipeCommonConfig
+		pipe.processor.CommonFields = pipe.PipeCommonConfig
+		propagateCommonFieldsInStruct(pipe.Gatherer, pipe.PipeCommonConfig)
+		propagateCommonFieldsInStruct(pipe.Sender, pipe.PipeCommonConfig)
+		propagateCommonFieldsInStruct(pipe.Processor, pipe.PipeCommonConfig)
+	}
+}
+
+// propagate the common fields to the sharing plugins.
+func propagateCommonFieldsInSharing(sharing *SharingConfig) {
+	propagateCommonFieldsInStruct(sharing, sharing.SharingCommonConfig)
+}
+
+// propagate the common fields to the fields that is one of `plugin.config` or `[]plugin.config` types.
+func propagateCommonFieldsInStruct(cfg interface{}, commonFields *config.CommonFields) {
+	v := reflect.ValueOf(cfg)
+	if v.Kind() == reflect.Ptr {
+		v = v.Elem()
+	}
+	for i := 0; i < v.Type().NumField(); i++ {
+		fieldVal := v.Field(i).Interface()
+		if arr, ok := fieldVal.([]plugin.Config); arr != nil && ok {
+			for _, pc := range arr {
+				propagateCommonFields(pc, commonFields)
+			}
+		} else if pc, ok := fieldVal.(plugin.Config); pc != nil && ok {
+			propagateCommonFields(pc, commonFields)
+		}
+	}
+}
+
+// propagate the common fields to the `plugin.config`.
+func propagateCommonFields(pc plugin.Config, cf *config.CommonFields) {
+	v := reflect.ValueOf(cf)
+	if v.Kind() == reflect.Ptr {
+		v = v.Elem()
+	}
+	t := v.Type()
+	for i := 0; i < t.NumField(); i++ {
+		if tagVal := t.Field(i).Tag.Get(config.TagName); tagVal != "" {
+			pc[strings.ToLower(config.CommonFieldsName)+"_"+tagVal] = v.Field(i).Interface()
+		}
+	}
+}
